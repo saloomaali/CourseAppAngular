130b1f6c4fbcd73b53f60168c28f9bb5d412bb29
Salooma <saloomaali2000@gmail.com>
2022-11-25 19:27:13 +0530

    route specified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddCourseComponent } from './add-course/add-course.component';
 import { ViewCourseComponent } from './view-course/view-course.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const myRoute:Routes = [{
+  path:"",
+  component:AddCourseComponent
+},
+{
+  path:"view",
+  component:ViewCourseComponent
+}
+
+]
 
 @NgModule({
   declarations: [
@@ -14,7 +26,8 @@ import { ViewCourseComponent } from './view-course/view-course.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
