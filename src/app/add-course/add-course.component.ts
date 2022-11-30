72d4d8ec92ba726e4d617566021066bf847ab017
Salooma <saloomaali2000@gmail.com>
2022-11-30 09:46:49 +0530

    api added to send data

--- src/app/add-course/add-course.component.ts
+++ src/app/add-course/add-course.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-add-course',
@@ -13,10 +14,36 @@ export class AddCourseComponent {
   courseDate = ""
   courseVenue = ""
 
+
+  constructor(private api:ApiService){}
+
   readValue = () => {
 
     let data:any = {"courseTitle":this.courseTitle,"courseDescription":this.courseDescription,"courseDuration":this.courseDuration,"courseDate":this.courseDate,"courseVenue":this.courseVenue}
     console.log(data)
+
+    this.api.addCourse(data).subscribe(
+
+      (response:any)=>{
+
+        console.log(response)
+        if (response.status == "success"){
+
+          alert ("Course added successfully")
+          this.courseTitle = ""
+          this.courseDescription =""
+          this.courseDate = ""
+          this.courseDuration = ""
+          this.courseVenue = ""
+          
+        }
+        else {
+          alert("something went wrong")
+        }
+      }
+
+     
+    )
 
   }
 
--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -13,5 +13,11 @@ export class ApiService {
     return this.http.get("https://mylinkurcodesapp.herokuapp.com/getcourses")
 
   }
+  
+  addCourse = (dataToSend:any) => {
+
+    return this.http.post("https://mylinkurcodesapp.herokuapp.com/addcourse" , dataToSend)
+
+  }
 
 }
